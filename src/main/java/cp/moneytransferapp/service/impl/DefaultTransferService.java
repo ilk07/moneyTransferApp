b90fe191de777a241a2bb85eaa92fd1900dc9220
ilk07 <ilk07@inbox.ru>
2023-05-08 18:10:10 +0300

    removed method setTransferJournal, finalized class fields, removed thrown of OutOfService exception, added new action to set cardToCardMoneyTransfer transfer status, added TransferLogger bean

--- src/main/java/cp/moneytransferapp/service/impl/DefaultTransferService.java
+++ src/main/java/cp/moneytransferapp/service/impl/DefaultTransferService.java
@@ -1,36 +1,34 @@
 package cp.moneytransferapp.service.impl;
 
-import cp.moneytransferapp.exception.OutOfService;
-import cp.moneytransferapp.logger.impl.TransferLogger;
 import cp.moneytransferapp.entities.CardToCardMoneyTransfer;
 import cp.moneytransferapp.entities.TransferConfirmation;
 import cp.moneytransferapp.entities.TransferStatus;
+import cp.moneytransferapp.logger.impl.TransferLogger;
 import cp.moneytransferapp.repository.impl.TransferRepositoryInMemory;
 import cp.moneytransferapp.service.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DefaultTransferService implements TransferService {
-    final TransferRepositoryInMemory repository;
-    protected TransferLogger transferJournal = TransferLogger.getInstance();
+    private final TransferRepositoryInMemory repository;
 
+    @Autowired
+    private final TransferLogger transferJournal;
 
     public DefaultTransferService(TransferRepositoryInMemory repository) {
         this.repository = repository;
+        this.transferJournal = TransferLogger.getInstance();
     }
 
     public CardToCardMoneyTransfer transferRequest(CardToCardMoneyTransfer cardToCardMoneyTransfer) {
-
         cardToCardMoneyTransfer.setVerificationCode(verificationCode());
-
+        cardToCardMoneyTransfer.setStatus(TransferStatus.AWAITING_CONFIRMATION);
         if (this.repository.saveTransfer(cardToCardMoneyTransfer)) {
-
             transferJournal.logToJournal(cardToCardMoneyTransfer.toString());
             return cardToCardMoneyTransfer;
-
         }
-
-        throw new OutOfService("Service unavailable");
+        throw new IllegalArgumentException("Unable to update verification code"); //код
     }
 
     public CardToCardMoneyTransfer confirmTransferOperation(TransferConfirmation transferConfirmation) {
@@ -44,21 +42,14 @@ public class DefaultTransferService implements TransferService {
                 if (this.repository.saveTransfer(cardToCardMoneyTransfer)) {
                     transferJournal.logToJournal(cardToCardMoneyTransfer.toString());
                     return cardToCardMoneyTransfer;
-                    
                 }
-                throw new OutOfService("Service unavailable, please, try again later");
-
             } else {
                 throw new IllegalArgumentException("Wrong verification code"); //код
             }
         }
         throw new IllegalArgumentException("Transfer declined, outdated");
-
     }
 
-    public void setTransferJournal(TransferLogger transferJournal) {
-        this.transferJournal = transferJournal;
-    }
 
     @Override
     public String verificationCode() {
