c456ce4791d958fc482dfca7f1622273de11023c
ilk07 <ilk07@inbox.ru>
2023-05-08 18:04:26 +0300

    redefined class fields to final, constructor moved to lombok annotations

--- src/main/java/cp/moneytransferapp/dto/cardtocard/TransferConfirmationDTO.java
+++ src/main/java/cp/moneytransferapp/dto/cardtocard/TransferConfirmationDTO.java
@@ -1,28 +1,23 @@
 package cp.moneytransferapp.dto.cardtocard;
 
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-
 @Getter
 @Setter
+@AllArgsConstructor
 public class TransferConfirmationDTO {
-
     @NotEmpty(message = "Empty operation id")
     @NotNull(message = "Operation id is null")
     @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", message = "Wrong operation Id")
-    private String operationId;
+    private final String operationId;
 
     @NotEmpty(message = "Empty code")
     @NotNull(message = "Code is null")
     @Pattern(regexp = "^[0-9]{4}$", message = "Wrong verification code")
-    private String code;
-
-    public TransferConfirmationDTO(){
-
-    }
-
+    private final String code;
 }
